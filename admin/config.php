fda4f1f5334d9d42738f94f104f26d1434fc0b02
Slava <ucheba00@gmail.com>
2017-03-28 14:25:59 -0600

    create repository fot the opencart learnig project

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart.loc/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart.loc/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart.loc/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/slvust/Documents/sites/opencart.loc/admin/');
+define('DIR_SYSTEM', '/Users/slvust/Documents/sites/opencart.loc/system/');
+define('DIR_IMAGE', '/Users/slvust/Documents/sites/opencart.loc/image/');
+define('DIR_LANGUAGE', '/Users/slvust/Documents/sites/opencart.loc/admin/language/');
+define('DIR_TEMPLATE', '/Users/slvust/Documents/sites/opencart.loc/admin/view/template/');
+define('DIR_CONFIG', '/Users/slvust/Documents/sites/opencart.loc/system/config/');
+define('DIR_CACHE', '/Users/slvust/Documents/sites/opencart.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/slvust/Documents/sites/opencart.loc/system/storage/download/');
+define('DIR_LOGS', '/Users/slvust/Documents/sites/opencart.loc/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/slvust/Documents/sites/opencart.loc/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/slvust/Documents/sites/opencart.loc/system/storage/upload/');
+define('DIR_CATALOG', '/Users/slvust/Documents/sites/opencart.loc/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart.loc/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/slvust/Documents/sites/opencart.loc/catalog/');
+define('DIR_SYSTEM', '/Users/slvust/Documents/sites/opencart.loc/system/');
+define('DIR_IMAGE', '/Users/slvust/Documents/sites/opencart.loc/image/');
+define('DIR_LANGUAGE', '/Users/slvust/Documents/sites/opencart.loc/catalog/language/');
+define('DIR_TEMPLATE', '/Users/slvust/Documents/sites/opencart.loc/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/slvust/Documents/sites/opencart.loc/system/config/');
+define('DIR_CACHE', '/Users/slvust/Documents/sites/opencart.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/slvust/Documents/sites/opencart.loc/system/storage/download/');
+define('DIR_LOGS', '/Users/slvust/Documents/sites/opencart.loc/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/slvust/Documents/sites/opencart.loc/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/slvust/Documents/sites/opencart.loc/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
